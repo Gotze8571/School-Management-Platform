09768a24929f28c3fd3fbf26f5fd0d3a01351f69
Mr Tella Adesina <tella.adesina@fcmb.com>
2020-08-04 10:57:17 +0100

    Add project files.

---
+++ MarketApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketApp.Models
+{
+    public class Product
+    {
+        public long ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductCategory { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
---
+++ MarketApp/Models/ServiceRendered.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarketApp.Models
+{
+    public class ServiceRendered
+    {
+        public long ServiceRenderedId { get; set; }
+        public string ServiceRenderedName { get; set; }
+        public string ServiceRenderedCategory { get; set; }
+        public DateTime ServiceRenderedDateTime { get; set; }
+    }
+}
